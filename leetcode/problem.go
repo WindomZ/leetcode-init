9e6a71b5fcf6c501884f9b54adeb2a125acad1da
WindomZ <git.windomz@gmail.com>
2017-09-28 23:39:56 +0800

    note: problem

--- leetcode/problem.go
+++ leetcode/problem.go
@@ -12,6 +12,7 @@ import (
 	"github.com/lunny/html2md"
 )
 
+// Problem the struct of leetcode problem.
 type Problem struct {
 	Index       int
 	URL         string
@@ -21,10 +22,13 @@ type Problem struct {
 	Difficulty  string
 }
 
+// Parse parses URL and constructs.
 func (p *Problem) Parse() error {
 	if p.URL == "" && p.Short == "" {
 		return errors.New("can't find the problem")
 	}
+
+	// URL
 	if p.URL == "" {
 		p.URL = fmt.Sprintf(
 			"https://leetcode.com/problems/%s/description/",
@@ -32,6 +36,7 @@ func (p *Problem) Parse() error {
 		)
 	}
 
+	// Short
 	if p.Short == "" {
 		if strings.HasPrefix(p.URL, "https://leetcode.com/problems/") {
 			p.Short = p.URL[30:]
@@ -44,6 +49,7 @@ func (p *Problem) Parse() error {
 		return err
 	}
 
+	// Name
 	p.Name = doc.Find(".question-title .row h3").First().Text()
 	if p.Name != "" {
 		idx := strings.Index(p.Name, ".")
@@ -51,22 +57,26 @@ func (p *Problem) Parse() error {
 		p.Name = strings.TrimSpace(p.Name[idx+1:])
 	}
 
+	// Description
 	p.Description, err = doc.Find("div.question-description").First().Html()
 	if err != nil {
 		return err
 	}
 	p.Description = strings.TrimSpace(p.Description)
 
+	// Difficulty
 	p.Difficulty = doc.Find("span.difficulty-label").First().Text()
 	p.Difficulty = strings.TrimSpace(p.Difficulty)
 
 	return nil
 }
 
+// ReadMe convert description to markdown.
 func (p *Problem) ReadMe() string {
 	return html2md.Convert(p.Description)
 }
 
+// OutputReadMe save to README.md with dir path.
 func (p *Problem) OutputReadMe(dir string) error {
 	return path.OverwriteFile(
 		filepath.Join(dir, "README.md"),
